8465562ecb9f6021330fe9a290b970ce6dd627be
Ryan Kerluke <146582651+ksfryan@users.noreply.github.com>
2024-11-06 16:19:04 +0000

    test 1

--- assignment.py
+++ assignment.py
@@ -1,1 +1,15 @@
-
+x = input("large or extra large:")
+if x == "large":
+ a = 6
+if x == "extra large":
+ a = 10
+    
+y = input("Enter amount of toppings:")
+if y == "1":
+a = a + 1
+if y == "1":
+a = a + 1.75
+if y == "1":
+a = a + 2.50
+if y == "1":
+a = a + 3.35
