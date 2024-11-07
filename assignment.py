da6a4b977d0ca4374e7b4f1d10be0db01fa8f61e
Ryan Kerluke <146582651+ksfryan@users.noreply.github.com>
2024-11-07 14:55:49 +0000

    php test

--- assignment.py
+++ assignment.py
@@ -3,13 +3,13 @@ print("Welcome to github Pizzaria!!")
 x = input("large or extra large: ")
 if x == "large":
   a = 6
-
+  
 elif x == "extra large" or "xl" or "xl" or "Extra Large":
   a = 10
 
 else:
-    print("Invalid size")
-    a = 0
+  print("Invalid size")
+  a = 0
 
 y = input("Enter amount of toppings:")
 if y == "1":
@@ -26,6 +26,7 @@ elif y == "4":
 
 else:
   print("Invalid number of toppings")
+
 
 c = "Your total with tax is" 
 h = round(float(a) * 1.13,2)  #rounding the number and adding tax
--- handler.php
+++ handler.php
@@ -1,23 +1,57 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Welcome Page</title>
+</head>
+
+<body>
+
+  <?php
+  $myVariable = "";
+  if (isset($_POST['myVariable'])) {
+    $myVariable = $_POST['myVariable'];
+  }
+  echo "<h1>My Program</h1>\n";
+  echo "<p>My Variable is = " . $myVariable . "</p>\n";
+  ?>
+
+</body>
+
 </html>
+
+<?php
+echo "Welcome to github Pizzaria!!\n";
+$x = readline("large or extra large: ");
+
+if ($x == "large") {
+  $a = 6;
+} elseif (in_array($x, ["extra large", "xl", "XL", "Extra Large"])) {
+  $a = 10;
+} else {
+  echo "Invalid size\n";
+  exit();  // End the program if an invalid size is entered
+}
+
+$y = readline("Enter amount of toppings: ");
+
+if ($y == "1") {
+  $a += 1;
+} elseif ($y == "2") {
+  $a += 1.75;
+} elseif ($y == "3") {
+  $a += 2.50;
+} elseif ($y == "4") {
+  $a += 3.35;
+} else {
+  echo "Invalid number of toppings\n";
+  exit();  // End the program if an invalid number of toppings is entered
+}
+
+$c = "Your total with tax is";
+$h = round($a * 1.13, 2);  // Rounding the number and adding tax
+echo "$c $h\n";
+?>
