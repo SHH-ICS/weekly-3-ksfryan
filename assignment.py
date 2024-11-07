9ab51ab9bc5d9f2c18c3654a59195cddbdffa784
Ryan Kerluke <146582651+ksfryan@users.noreply.github.com>
2024-11-07 03:35:06 +0000

    final

--- assignment.py
+++ assignment.py
@@ -1,15 +1,32 @@
-x = input("large or extra large:")
+#The variable a is determined depending on if the input is large and extra large if large then a = 6.
+print("Welcome to github Pizzaria!!")
+x = input("large or extra large: ")
 if x == "large":
- a = 6
-if x == "extra large":
- a = 10
-    
+  a = 6
+
+elif x == "extra large" or "xl" or "xl" or "Extra Large":
+  a = 10
+
+else:
+    print("Invalid size")
+    a = 0
+
 y = input("Enter amount of toppings:")
 if y == "1":
-a = a + 1
-if y == "1":
-a = a + 1.75
-if y == "1":
-a = a + 2.50
-if y == "1":
-a = a + 3.35
+  a = a + 1
+  
+elif y == "2":
+  a = a + 1.75
+  
+elif y == "3":
+  a = a + 2.50
+  
+elif y == "4":
+  a = a + 3.35
+
+else:
+  print("Invalid number of toppings")
+
+c = "Your total with tax is" 
+h = round(float(a) * 1.13,2)  #rounding the number and adding tax
+print(c,h)
